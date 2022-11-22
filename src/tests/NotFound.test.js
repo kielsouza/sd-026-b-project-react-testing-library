e8932e89ac34a370fb6a0ad1e23042005865293f
kielsouza <kielsouza115@hotmail.com>
2022-11-22 17:19:41 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../pages/NotFound';
+
+describe('Testando Componente NotFound', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found.', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundText = screen.getByRole('heading', {
+      level: 2,
+      name: /page requested not found/i,
+    });
+    expect(notFoundText).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem.', () => {
+    renderWithRouter(<NotFound />);
+    const notFoundImg = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
