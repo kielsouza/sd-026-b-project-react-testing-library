7ae2e5f401f164f9d59e0ebb1111554c5945439b
kielsouza <kielsouza115@hotmail.com>
2022-11-22 17:12:55 -0300

    Requisito 3

--- src/tests/FavoritePokemon.test.js
+++ src/tests/FavoritePokemon.test.js
@@ -1,1 +1,29 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import FavoritePokemon from '../pages/FavoritePokemon';
+
+describe('Testando Componente FavoritePokemon', () => {
+  test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha Pokémon favoritos.', () => {
+    renderWithRouter(<FavoritePokemon />);
+    /* const localStoragePokemons = localStorage.getItem('favoritePokemonIds');
+    if (localStoragePokemons === 0) {
+      const noPokemons = screen.getByText(/no favorite pokémon found/i);
+      expect(noPokemons).toBeInTheDocument();
+    } else {
+      const havePokemons = screen.getByRole('heading', {
+        level: 2,
+        name: /favorite pokémon/i,
+      });
+      expect(havePokemons).toBeInTheDocument();
+    } */
+    const noPokemons = screen.getByText(/no favorite pokémon found/i);
+    expect(noPokemons).toBeInTheDocument();
+  });
+  test('Teste se são exibidos todos os cards de Pokémon favoritados.', () => {
+    renderWithRouter(<FavoritePokemon />);
+    const localStoragePokemons = localStorage.getItem('favoritePokemonIds');
+    if (localStoragePokemons > 0) {
+      expect(localStoragePokemons).toBeInTheDocument();
+    }
+  });
+});
